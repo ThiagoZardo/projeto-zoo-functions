975c16948df232ef83be4e20f0b6bded6d3512a8
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-01-25 17:12:51 -0300

    Requisito 01

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids === undefined) {
+    return [];
+  }
+  return species.filter((specie) => ids.find((idObjeto) => (specie.id === idObjeto)));
 }
 
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 module.exports = getSpeciesByIds;
