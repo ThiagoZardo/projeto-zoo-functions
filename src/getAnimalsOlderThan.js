c93bf94a56566eb0e61972ab07c3c0d0db3534c0
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-01-25 20:28:17 -0300

    Requisito 02

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+//O Sumo e o Imar Mendes me ajudaram a entender a lógica do retorno do primeiro find que é usado na segunda HOF (every). 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return species.find((specie) => specie.name === animal)
+    .residents.every((resident) => resident.age >= age);
 }
+
+console.log(getAnimalsOlderThan('otters', 7));
 
 module.exports = getAnimalsOlderThan;
