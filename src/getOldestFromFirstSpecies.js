abf19b33aa94897b9c8643825ba2f669aeee7546
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-01-26 15:54:39 -0300

    Iniciando Requisito 08

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const idFirst = employees
+  .find((employee) => employee.managers.includes(id)).responsibleFor[0];
+
+  const oldAge = species.find((specie) => specie.id === idFirst).residents 
+    .reduce((acc, curr) => (curr.age > acc ? curr : acc), 0)
 }
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,31 @@
+const { hours, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const daysOfWeek = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
+const animals = ['lions', 'tigers', 'bears', 'penguins', 'otters', 'frogs', 'snakes', 'elephants', 'giraffes']
+
+let obj = {};
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  // Sem parâmetros, retorna os horários para cada dia e quais animais estarão disponíveis;
+  if (!scheduleTarget){
+    daysOfWeek.forEach((element) => {
+      if (element !== 'Monday') {
+        obj[element] = {
+          'officeHour': `Open from ${hours[element].open}am until ${hours[element].close}pm`,
+          'exhibition': species.filter((specie) => specie.availability.includes(element))
+          .map((animal) => animal.name),
+        }
+      } else {
+          obj[element] = {
+            'officeHour': 'CLOSED',
+            'exhibition': 'The zoo will be closed!',
+          }  
+        }
+    });
+  }
+  return obj;
 }
 
+console.log(getSchedule());
 module.exports = getSchedule;
