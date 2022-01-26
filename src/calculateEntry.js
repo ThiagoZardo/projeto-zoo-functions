30b8233693b987f82e1681f6c642d3fdf1bb26b9
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-01-26 01:59:41 -0300

    Requisito 06

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,13 +1,42 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
+const entrants = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
+
+function countEntrants(array) {
+  const quantidadePessoas = { child: 0, adult: 0, senior: 0 };
+  array.forEach((element) => {
+    if (element.age < 18) {
+      quantidadePessoas.child += 1;
+    }
+    if (element.age >= 18 && element.age < 50) {
+      quantidadePessoas.adult += 1;
+    }
+    if (element.age >= 50) {
+      quantidadePessoas.senior += 1;
+    }
+  });
+  return quantidadePessoas;
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
+function calculateEntry(array) {
+  if (!array || Object.keys(array).length === 0) {
+    return 0;
+  }
+  const objRetornado = countEntrants(array);
+  let valorTotal = 0;
+  valorTotal += objRetornado.child * prices.child;
+  valorTotal += objRetornado.adult * prices.adult;
+  valorTotal += objRetornado.senior * prices.senior;
+  return valorTotal;
 }
-
-// Iniciando projeto
+console.log(calculateEntry(entrants));
 
 module.exports = { calculateEntry, countEntrants };
