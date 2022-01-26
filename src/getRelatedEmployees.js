c46b246c60b1e9541d7481f9ff967a2a62599948
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-01-25 22:40:50 -0300

    Requisito 04

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
+// const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((element) => element.managers.includes(id));
 }
-
+// Imar Mendes me ajudou a entender o uso do Includes()
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    const employeesLet = employees.filter((employee) => employee.managers.includes(managerId));
+    return employeesLet.map((employee) => `${employee.firstName} ${employee.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
