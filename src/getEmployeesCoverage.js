76bd8e780c7e106f3f19b9d11d836ba048767596
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-01-28 01:36:47 -0300

    Ajustei alguns erros do lint no Req 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -7,35 +7,37 @@ const retornaListaFuncionarios = () => {
   employees.forEach((element) => {
     const obj = {
       id: element.id,
-      fullName: element.firstName + ' ' + element.lastName,
-      species: species.filter((specie) => element.responsibleFor.includes(specie.id)).map((nomeAnimal) => nomeAnimal.name),
-      locations: species.filter((specie) => element.responsibleFor.includes(specie.id)).map((nomeAnimal) => nomeAnimal.location),
+      fullName: `${element.firstName} ${element.lastName}`,
+      species: species.filter((specie) => element.responsibleFor
+        .includes(specie.id)).map((nomeAnimal) => nomeAnimal.name),
+      locations: species.filter((specie) => element.responsibleFor
+        .includes(specie.id)).map((nomeAnimal) => nomeAnimal.location),
     };
     array.push(obj);
-  })
+  });
   return array;
-}
+};
 
 const retornaObjetoFuncionario = (objeto) => {
   const pessoasFuncionarias = retornaListaFuncionarios();
   if (objeto !== objeto.name) {
-    console.log('Objeto com ID')
-  return pessoasFuncionarias.find((element) => element.id
-  .includes(objeto.id));
+    console.log('Objeto com ID');
+    return pessoasFuncionarias.find((element) => element.id
+      .includes(objeto.id));
   }
   return pessoasFuncionarias.find((element) => element.fullName
-  .includes(objeto.name));
-}
+    .includes(objeto.name));
+};
 
 function getEmployeesCoverage(parametro) {
-  if(!parametro) {
+  if (!parametro) {
     return retornaListaFuncionarios();
   }
-  if(typeof parametro === 'object') {
+  if (typeof parametro === 'object') {
     return retornaObjetoFuncionario(parametro);
   }
   throw new Error(/^Informações inválidas$/);
 }
 
-console.log(getEmployeesCoverage({ name: 'Spry' }))
+console.log(getEmployeesCoverage({ name: 'Spry' }));
 module.exports = getEmployeesCoverage;
