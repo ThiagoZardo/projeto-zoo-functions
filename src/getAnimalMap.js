82d9934a8235a6aa876b06695f06b158fa49ab6a
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-01-29 00:09:34 -0300

    Requisito 7 test 01

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,21 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+const localizacao = ['NE', 'NW', 'SE', 'SW'];
 
+const mapaAnimais = () => {
+  let obj = {};
+  localizacao.forEach((element) => {
+    obj[element] = species.filter((specie) => specie.location
+      .includes(element)).map((nameSpecie) => nameSpecie.name);
+  });
+  return obj;
+};
+
+function getAnimalMap(options) {
+  if (!options) {
+    return mapaAnimais();
+  }
+}
+console.log(getAnimalMap());
 module.exports = getAnimalMap;
